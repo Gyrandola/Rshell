9280c2b291c9e1c32d69f62d870b2af51eea5023
Gyran <62253058+Gyrandola@users.noreply.github.com>
2024-03-02 01:31:41 +0100

    implemented mkdir

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
-use std::process::{Command, ExitCode};
-use std::{env, io};
+use std::process::{Command, exit};
+use std::{env, fs, io};
 use std::path::Path;
 
-const BUILTIN_COMMANDS: &[&str] = &["cd", "exit"];
+const BUILTIN_COMMANDS: &[&str] = &["cd", "exit", "mkdir"];
 
 
-fn main() -> ExitCode{
-    // Config
-
-    // Commands
+fn main(){
     rshell_loop();
-
-    // Cleanup
-
-    ExitCode::SUCCESS
+    exit(0);
 }
 
 fn rshell_loop() {
     let mut line: String;
-    let mut args: Vec<String>;// char **args
+    let mut args: Vec<String>;
 
     loop{
+        // Print current dir every line
         eprint!("{}> ",env::current_dir().unwrap().display());
+
+        // Read raw line from input
         line = rshell_read_line();
+
+        // Tokenize it
         args = rshell_tokenize(line);
+
+        // Execute it and handle errors
         if let Err(err) = rshell_execute(args) {
             eprintln!("Error: {}\n", err);
-            //break;
         }
 
     }
@@ -53,8 +53,6 @@ fn rshell_tokenize(line: String) -> Vec<String> {
         .map(ToOwned::to_owned)
         .collect();
 
-    //println!("Tokens: {}", tokens.join(", ")); //DEBUG
-
     return tokens;
 }
 
@@ -77,18 +75,29 @@ fn rshell_launch(args : Vec<String>) ->Result<(), std::io::Error>{
 //Handle our shell builtins
 fn rshell_builtin(args: &[String]) -> Result<(), std::io::Error> {
     match args[0].as_str() {
+
         "cd" => {
             if args[1].is_empty() {
                 eprintln!("Error: expected argument to cd\n")
             }else{
                 let root = Path::new(&args[1]);
-                if root.exists(){
-                    env::set_current_dir(&root).expect("Failed to change directory\n");
-                }else{
-                    eprintln!("Error: directory '{}' does not exist\n", root.display());
-                }
 
+                if root.exists(){
+                    env::set_current_dir(Path::new(&args[1])).expect("Failed to change directory\n");
+                }else{
+                    eprintln!("Error: cannot find specified path\n");
+                }
             }
+            Ok(())
+        }
+
+        "mkdir" => {
+            if args[1].is_empty() {
+                eprintln!("Error: expected argument to mkdir\n");
+            }else{
+                fs::create_dir_all(&args[1]).expect("Failed to create directories");
+            }
+
             Ok(())
         }
 
@@ -96,7 +105,9 @@ fn rshell_builtin(args: &[String]) -> Result<(), std::io::Error> {
             std::process::exit(0);
         }
 
-        //I hate this
+
+
+        // Ugly
         _ => Err(std::io::Error::new(std::io::ErrorKind::NotFound, "builtin not found\n")),
     }
 }
